c77d0b27c2d5ce26c99d07277e5e03407da145a3
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-04-09 23:27:12 +0200

    Home: Add basic navigation

---
+++ src/screens/Home.js
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import ColorContext from "../utils/ColorContext";
+import Projects from ".//Projects";
+import Members from "./Members";
+
+const Tab = createBottomTabNavigator();
+
+export default function Home() {
+  const [color] = useContext(ColorContext);
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: color,
+      }}
+    >
+      <Tab.Screen
+        name="Projects"
+        component={Projects}
+        options={{
+          tabBarIcon: (props) => (
+            <MaterialCommunityIcons name="briefcase-account" {...props} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Members"
+        component={Members}
+        options={{
+          tabBarIcon: (props) => (
+            <MaterialCommunityIcons name="briefcase-account" {...props} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
