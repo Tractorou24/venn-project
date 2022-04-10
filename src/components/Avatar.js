41ac409500799c56d4fdeabc2376cfe4e3f8f08b
LMERLAUD <louis.merlaud@ynov.com>
2022-04-10 20:42:23 +0200

    Avatar: Add UI

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 
-const SIZE = 48;
+const SIZE = 100;
+const screen = Dimensions.get("screen");
 
 export default function Avatar({ color = "#000", label, size = SIZE }) {
+  const styles = createStyles({ color });
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={styles.container}>
+      <Text style={styles.initials}>{label}</Text>
     </View>
   );
 }
+
+const createStyles = ({ color }) =>
+  StyleSheet.create({
+    container: {
+      flex: 0,
+      width: SIZE,
+      height: SIZE,
+      borderRadius: 15,
+      borderWidth: 8,
+      borderStyle: "solid",
+      borderColor: color,
+      alignItems: "center",
+      justifyContent: "center",
+      alignSelf: "center",
+      marginTop: screen.height / 5.5,
+    },
+    initials: {
+      fontSize: SIZE / 2.5,
+      textTransform: "uppercase",
+      color: color,
+      fontWeight: "bold",
+    },
+  });
